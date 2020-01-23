d71d1b47fa718ab67569d4dfeb4f967d4607a9d7
volodin-victor <dev@volodin.org>
2020-01-23 01:13:10 -0800

    Clean up code
    Signed-off-by: volodin-victor <dev@volodin.org>

--- public/index.php
+++ public/index.php
@@ -1,45 +1,16 @@
 <?php
 
-function getLang(Request $request, $default) {
-    return
-        !empty($request->getQueryParams()['lang']) ? $request->getQueryParams()['lang'] :
-            (!empty($request->getCookies()['lang']) ? $request->getCookies()['lang'] :
-                (!empty($request->getSession()['lang']) ? $request->getSession()['lang'] :
-                    (!empty($request->getServerParam()['HTTP_ACCEPT_LANGUAGE']) ?substr($request->getServerParam()['HTTP_ACCEPT_LANGUAGE'], 0, 2) :
-                        $default)));
-}
-session_start();
+use Framework\Http\Request;
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
 
-class Request
-{
-    public function getQueryParams() : array
-    {
-        return $_GET;
-    }
-
-    public function getCookies() : array
-    {
-        return $_COOKIE;
-    }
-
-    public function getSession() :array
-    {
-        return $_SESSION;
-    }
-
-    public function getServerParam()
-    {
-        return $_SERVER;
-    }
-}
-
+### Initialization
 
 $request = new Request();
 
 
-$name = $_GET['name'] ?? 'Guest';
-$lang = getLang($request,'en');
+### Action
 
+$name = $request->getQueryParams()['name'] ?? 'Guest';
 header('X-Developer: Victor Volodin');
-
-echo 'Hello, ' . $name . '! Your language is ' . $lang;
+echo 'Hello, ' . $name . '!';
